c542a37b0b23a593066413f306be3fc63bb1f49c
Hicham <hrifton@hotmail.com>
2019-09-06 08:22:53 +0200

    formate

--- Front/src/app/interventions/interventions.component.ts
+++ Front/src/app/interventions/interventions.component.ts
@@ -1,20 +1,29 @@
-import { UserService } from '../Service/user.service';
-// tslint:disable-next-line: import-spacing
-import { AnalyseMixIntermaintComponent } from '../analyse-mix-intermaint/analyse-mix-inter-maint.component';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from "@angular/core";
 
-import { InterventionService } from './../Service/intervention.service';
-import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import { User } from '../Class/user';
-import { ListInterventionComponent } from './list-intervention/list-intervention.component';
+/**
+ * Import service
+ */
+import { UserService } from "../Service/user.service";
+import { InterventionService } from "./../Service/intervention.service";
+import { DateMaintenanceService } from "../Service/dateMaintenance.service";
+import { DomaineService } from "../Service/domaine.service";
+/**
+ * import component
+ */
+import { AnalyseMixIntermaintComponent } from "../analyse-mix-intermaint/analyse-mix-inter-maint.component";
+import { ListInterventionComponent } from "./list-intervention/list-intervention.component";
 
-import Intervention from '../Class/Intervention';
-import { DateMaintenanceService } from '../Service/dateMaintenance.service';
-import * as moment from 'moment';
-import { DomaineService } from '../Service/domaine.service';
+/**
+ * import class
+ */
+import { User } from "../Class/user";
+import Intervention from "../Class/Intervention";
+/** import moment outil parsing et modification  */
+import * as moment from "moment";
 
 @Component({
-  selector: 'app-interventions',
-  templateUrl: './interventions.component.html',
+  selector: "app-interventions",
+  templateUrl: "./interventions.component.html",
   encapsulation: ViewEncapsulation.None
 })
 export class InterventionsComponent implements OnInit {
@@ -30,7 +39,13 @@ export class InterventionsComponent implements OnInit {
 
   @ViewChild(AnalyseMixIntermaintComponent)
   AnalyseMixIntermaint: AnalyseMixIntermaintComponent;
-
+  /**
+   *
+   * @param is service intervention
+   * @param us service user
+   * @param ds service dateMaintenance
+   * @param doms service domaine
+   */
   constructor(
     private is: InterventionService,
     private us: UserService,
@@ -39,29 +54,37 @@ export class InterventionsComponent implements OnInit {
   ) {
     this.interventions = [];
     this.maintenance = [];
-    console.log('compoment parent: Constructor');
   }
 
+  /**
+   * @param $event
+   * est appellé depuis le component Liste Intervention pour mise a jour de la liste intervention
+   *  unshift ajoute data de event dans le tableau interventions
+   */
   update($event) {
-        this.interventions.unshift($event);
-          this.interentionList.refreshInterventionTable();
+    this.interventions.unshift($event);
+    this.interentionList.refreshInterventionTable();
     // this.HistoricIntervention.refreshChart();
   }
   check($event) {
-    console.log('inter closed :', $event);
+    console.log("inter closed :", $event);
   }
 
   ngOnInit() {
+    /**
+     * assignation statut de l'utilisateur
+     */
     this.userDetails = this.us.getStatus();
-    // gestion Du type d'utilisateur
-
-    if (this.userDetails === 'user') {
+    /**
+     * gestion Du type d'utilisateur
+     */
+    if (this.userDetails === "user") {
       this.is
         .getInterventionsByUser(this.us.getFullName())
         .subscribe((data: Intervention[]) => {
           this.interventions = data;
         });
-    } else if (this.userDetails === 'tech') {
+    } else if (this.userDetails === "tech") {
       this.is
         .getInterventionsBytech(this.us.getFullName())
         .subscribe((data: Intervention[]) => {
@@ -74,29 +97,29 @@ export class InterventionsComponent implements OnInit {
             if (element.resultat.length <= 0) {
               const inter = {
                 _id: element.idMaintenance,
-                day: moment(element.StartTime).format('DD/MM/YYYY'),
-                departement: '',
-                description: '',
-                locality: '',
-                priority: 'Medium',
-                status: 'In process',
-                tech: '',
-                type: 'Maintenance',
-                user: ''
+                day: moment(element.StartTime).format("DD/MM/YYYY"),
+                departement: "",
+                description: "",
+                locality: "",
+                priority: "Medium",
+                status: "In process",
+                tech: "",
+                type: "Maintenance",
+                user: ""
               };
               data.push(inter);
             } else {
               const inter = {
                 _id: element.idMaintenance,
-                day: moment(element.StartTime).format('DD/MM/YYYY'),
+                day: moment(element.StartTime).format("DD/MM/YYYY"),
                 departement: element.resultat[0].executor,
                 description: element.resultat[0].description,
-                locality: '',
-                priority: 'Medium',
-                status: 'In process',
-                tech: '',
-                type: 'Maintenance',
-                user: ''
+                locality: "",
+                priority: "Medium",
+                status: "In process",
+                tech: "",
+                type: "Maintenance",
+                user: ""
               };
               data.push(inter);
             }
@@ -108,15 +131,25 @@ export class InterventionsComponent implements OnInit {
         });
       });
     }
-
+    /**
+     * return liste des technicien
+     */
     this.us.getUserTech().subscribe((data: User[]) => {
       this.techs = data;
     });
+    /**
+     * return liste des deparement
+     */
     this.doms.getAll().subscribe(data => {
       this.domaine = data;
     });
   }
-
+  /**
+   *
+   * @param domaine
+   * @param intervention
+   * compte le nombre d'intervention par domaine (corp de métier)
+   */
   compteDomaine(domaine, intervention) {
     console.log(intervention.length);
     for (let index = 0; index < domaine.length; index++) {
@@ -124,7 +157,7 @@ export class InterventionsComponent implements OnInit {
       this.compte[index] = { name, nb: 0 };
     }
     intervention.forEach(element => {
-      if (element.status !== 'Canceled' && element.status !== 'Closed') {
+      if (element.status !== "Canceled" && element.status !== "Closed") {
         for (const key in this.compte) {
           if (this.compte.hasOwnProperty(key)) {
             if (this.compte[key].name === element.domaine) {
