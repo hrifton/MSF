4d1a39d335ff4b2b0852dc7a7e57bec80a138070
Unknown <hrifton@hotmail.com>
2019-09-20 13:44:21 +0200

    Auto stash before merge of "master" and "Intervention-Back"

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { Routes, RouterModule } from "@angular/router";
 /**
  * importation des component mere de chaque module
  */
-import { MaintenancesComponent } from "./maintenances/maintenances.component";
-import { InterventionsComponent } from "./interventions/interventions.component";
-import { LoginComponent } from "./login/login.component";
-import { UserComponent } from "./user/user.component";
-import { SettingsComponent } from "./settings/settings.component";
+import { MaintenancesComponent } from './maintenances/maintenances.component';
+import { InterventionsComponent } from './interventions/interventions.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { SettingsComponent } from './settings/settings.component';
 //import { HistoricComponent } from './historic/historic.component';
 //import { DepartementComponent } from './setting/departement/departement.component';
 //import { TechnicienComponent } from './setting/technicien/technicien.component';
@@ -20,20 +20,17 @@ import { SettingsComponent } from "./settings/settings.component";
  * canActivate securisation d'acces a la route selon l'authentification et au niveau de droit
  */
 
-const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "signUp", component: UserComponent },
-  { path: "login", component: LoginComponent },
-  {
-    path: "interventions",
-    component: InterventionsComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: "maintenances",
-    component: MaintenancesComponent,
-    canActivate: [AuthGuard]
-  },
+/**
+ * Routes vers entrée de chaque component
+ * canActivate securisation d'acces a la route selon l'authentification et au niveau de droit
+ */
+
+ const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'signUp', component: UserComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'interventions', component: InterventionsComponent, canActivate: [AuthGuard] },
+  { path: 'maintenances', component: MaintenancesComponent, canActivate: [AuthGuard] },
   //{ path: 'historic', component: HistoricComponent, canActivate: [AuthGuard]},
   //{ path: 'setTec', component: TechnicienComponent, canActivate: [AuthGuard] },
   //{ path: 'setMet', component: CorpsDeMetierComponent, canActivate: [AuthGuard] },
