8775454d4640b5781dac272910946270fa8094fc
Hicham <hrifton@hotmail.com>
2019-09-22 17:36:00 +0200

    probleme chart Résolut

--- Front/src/app/analyse-mix-intermaint/analyse-intervention/analyse-intervention.component.ts
+++ Front/src/app/analyse-mix-intermaint/analyse-intervention/analyse-intervention.component.ts
@@ -1,132 +1,103 @@
-import { Component, OnInit, Input, ViewChild, SimpleChanges } from '@angular/core';
-import { ChartComponent, IAccLoadedEventArgs, AccumulationTheme } from '@syncfusion/ej2-angular-charts';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  SimpleChanges
+} from "@angular/core";
+import {
+  ChartComponent,
+  IAccLoadedEventArgs,
+  AccumulationTheme
+} from "@syncfusion/ej2-angular-charts";
 
 @Component({
-  selector: 'app-analyse-intervention',
-  templateUrl: './analyse-intervention.component.html',
-  styleUrls: ['./analyse-intervention.component.scss']
+  selector: "app-analyse-intervention",
+  templateUrl: "./analyse-intervention.component.html",
+  styleUrls: ["./analyse-intervention.component.scss"]
 })
 export class AnalyseInterventionComponent implements OnInit {
-
-
   @Input() analyseIntervention;
-  constructor() {
-    this.analyseIntervention = [];
-
-  }
-  @ViewChild('chart') public chart: ChartComponent;
+  constructor() {}
+  @ViewChild("chart") public chart: ChartComponent;
   public status = { open: 0, close: 0, canceled: 0, waiting: 0 };
   // tslint:disable-next-line: ban-types
   private piedata: Object[];
   public legendSettings: Object;
-  public map: Object = 'fill';
+  public map: Object = "fill";
   public datalabel: Object;
   public open: number;
   public close: number;
 
-
   ngOnInit() {
+    this.getChart(this.analyseIntervention);
   }
   public load(args: IAccLoadedEventArgs): void {
-    let selectedTheme: string = location.hash.split('/')[1];
-    selectedTheme = selectedTheme ? selectedTheme : 'Material';
-    args.accumulation.theme = (selectedTheme.charAt(0).toUpperCase() + selectedTheme.slice(1)).replace(/-dark/i, 'Dark') as AccumulationTheme;
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-
-   this.getNumberOpenClose(changes.analyseIntervention.currentValue);
-   this.analyseIntervention = changes.analyseIntervention.currentValue;
+    let selectedTheme: string = location.hash.split("/")[1];
+    selectedTheme = selectedTheme ? selectedTheme : "Material";
+    args.accumulation.theme = (
+      selectedTheme.charAt(0).toUpperCase() + selectedTheme.slice(1)
+    ).replace(/-dark/i, "Dark") as AccumulationTheme;
   }
 
- public refreshChart() {
-   
-   this.getNumberOpenClose(this.analyseIntervention);
-   this.chart.refresh();
-   console.log("refresh internvention")
-  }
-
-  getNumberOpenClose(data) {
-
-    let open = 0;
-    let canceled = 0;
-    let close = 0;
-    let waiting = 0;
-    data.forEach(element => {
-      switch (element.status) {
-        case 'In progress':
-          open++;
-          break;
-        case 'Closed':
-          close++;
-          break;
-        case 'Canceled':
-          canceled++;
-          break;
-        case 'In progress':
-          open++;
-          break;
-
-        default:
-          waiting++;
-          break;
-      }
-
-    });
-    this.status.open = open;
-    this.status.close = close;
-    this.status.canceled = canceled;
-    this.status.waiting = waiting;
-
-    this.getChart(this.status);
-  }
   getChart(data) {
-
     this.piedata = [
       // tslint:disable-next-line:max-line-length
       {
-        x: 'Closed : ' + data.close,
-        y: Math.round((data.close * 100) / (data.open + data.close + data.waiting + data.canceled)),
-        text: 'closed',
-        fill: '#d9480f'
+        x: "Closed : " + data.close,
+        y: Math.round(
+          (data.close * 100) /
+            (data.open + data.close + data.waiting + data.canceled)
+        ),
+        text: "closed",
+        fill: "#d9480f"
       },
       {
-        x: 'Open :' + data.open,
-        y: Math.round((data.open * 100) / (data.open + data.close + data.waiting + data.canceled)),
-        text: 'open :' + data.open,
-        fill: '#51cf66'
+        x: "Open :" + data.open,
+        y: Math.round(
+          (data.open * 100) /
+            (data.open + data.close + data.waiting + data.canceled)
+        ),
+        text: "open :" + data.open,
+        fill: "#51cf66"
       },
       {
-        x: 'Waiting :' + data.waiting,
-        y: Math.round((data.waiting * 100) / (data.open + data.close + data.waiting + data.canceled)),
-        text: 'waiting :' + data.waiting,
-        fill: '#fab005'
+        x: "Waiting :" + data.waiting,
+        y: Math.round(
+          (data.waiting * 100) /
+            (data.open + data.close + data.waiting + data.canceled)
+        ),
+        text: "waiting :" + data.waiting,
+        fill: "#fab005"
       },
       {
-        x: 'Canceled :' + data.canceled,
-        y: Math.round((data.canceled * 100) / (data.open + data.close + data.waiting + data.canceled)),
-        text: 'canceled :' + data.canceled,
-        fill: '#ffd8a8'
-      },
-
+        x: "Canceled :" + data.canceled,
+        y: Math.round(
+          (data.canceled * 100) /
+            (data.open + data.close + data.waiting + data.canceled)
+        ),
+        text: "canceled :" + data.canceled,
+        fill: "#ffd8a8"
+      }
     ];
 
-    this.datalabel = { visible: true, name: 'text', position: 'Outside' };
+    this.datalabel = { visible: true, name: "text", position: "Outside" };
 
     this.legendSettings = {
       visible: true
     };
-
+    this.chart.refresh();
   }
-  @ViewChild('pie') public pie: ChartComponent;
+  @ViewChild("pie") public pie: ChartComponent;
 
   // custom code end
-  public center: Object = { x: '50%', y: '50%' };
+  public center: Object = { x: "50%", y: "50%" };
   public startAngle = 0;
   public endAngle = 360;
   public explode = true;
   public enableAnimation = false;
   public tooltip: Object = {
     enable: true,
-    format: '${point.x} : <b>${point.y}%</b>'
+    format: "${point.x} : <b>${point.y}%</b>"
   };
 }
